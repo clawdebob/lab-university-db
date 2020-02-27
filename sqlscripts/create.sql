e058b374d03f6421ab0636870c2bff4c0a0b031e
Mokrushin Andrey <mok938@gmail.com>
2020-02-27 22:18:05 +0300

    finished lab-2 (#1)

---
+++ sqlscripts/create.sql
@@ -0,0 +1,57 @@
+create table study_group (
+    id int not null,
+    name text,
+    primary key (id)
+);
+
+create table student (
+    id int not null,
+    surname text,
+    name text,
+    second_name text,
+    study_group_id int,
+    primary key (id),
+    foreign key (study_group_id) references study_group(id)
+);
+
+create table subject (
+    id int not null,
+    name text,
+    short_name text,
+    primary key (id)
+);
+
+create table exam_type (
+    id int not null,
+    type text,
+    primary key (id)
+);
+
+create table study_plan (
+    id int not null,
+    subject_id int,
+    exam_type_id int,
+    primary key (id),
+    foreign key (subject_id) references subject(id),
+    foreign key (exam_type_id) references exam_type(id)
+);
+
+create table mark (
+    id int not null,
+    name text,
+    value text,
+    primary key (id)
+);
+
+create table journal (
+    id int not null,
+    student_id int,
+    study_plan_id int,
+    in_time bit,
+    count int,
+    mark_id int,
+    primary key (id),
+    foreign key (student_id) references student(id),
+    foreign key (study_plan_id) references study_plan(id),
+    foreign key (mark_id) references mark(id)
+);
